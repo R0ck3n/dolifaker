5870e4696b1be2ec31c7917a8b8b2b8f48779e24
Nico <r0ck3n@hotmail.com>
2026-01-23 14:35:08 +0100

    first commit

---
+++ src/main/java/com/dolibarrfaker/dolifaker/DolifakerApplication.java
@@ -0,0 +1,13 @@
+package com.dolibarrfaker.dolifaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolifakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolifakerApplication.class, args);
+	}
+
+}
